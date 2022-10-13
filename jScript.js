e3663cf1f9e78c924de99c006e2f37f0c6e2a490
240jk <jkubis@alunmi.nmu.edu>
2022-10-13 18:27:46 -0400

    first commit

---
+++ jScript.js
@@ -0,0 +1,18 @@
+
+var firstName = "Joe";
+var lastName = "Kubis";
+
+let fullName = name.concat(firstName, lastName);
+
+console.log(fullName);
+
+var populationDetroit = "672351";
+
+console.log(populationDetroit);
+
+let xPopulation = populationDetroit * 3;
+console.log(xPopulation);
+
+var javaScriptIsCool
+let javaScriptIsCool = Boolean(true);
+
